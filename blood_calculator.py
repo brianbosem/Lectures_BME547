489977b606de4ab9b0a61428df5dcfb9b71d7249
Brian Bosem <bab97@duke.edu>
2021-08-30 14:08:34 -0400

    Expanded interface to include LDL information

--- blood_calculator.py
+++ blood_calculator.py
@@ -4,12 +4,15 @@ def interface():
     while keep_running:
         print("Make a choice")
         print("1 = HDL Analysis")
+        print("2 = LDL Analysis")
         print("9 - Quit")
         choice = int(input("Make a choice: "))
         if choice == 9:
             keep_running = False
         elif choice == 1:
-                HDL_Driver()
+            HDL_Driver()
+        elif choice == 2:
+            LDL_Driver()
     print(choice)
     return choice
 
@@ -33,6 +36,29 @@ def hdl_analysis(HDL_value):
 
 def hdl_output(HDL_value, HDL_character):
     print("The HDL value of {} is considered {}".format(HDL_value, HDL_character))
-    
+
+
+def LDL_Driver():
+    LDL_value = ldl_input()
+    LDL_character = ldl_analysis(LDL_value)
+    ldl_output(LDL_value, LDL_character)
+
+def ldl_input():
+    ldl_value = int(input("Enter LDL value: "))
+    return ldl_value
+
+def ldl_analysis(LDL_value):
+    if LDL_value < 130:
+        return "Normal"
+    elif 130 <= LDL_value < 159:
+        return "Borderline High"
+    elif 160 < LDL_value < 189:
+        return "High"
+    else:
+        return "Very High" 
+
+def ldl_output(LDL_value, LDL_character):
+    print("The LDL value of {} is considered {}".format(LDL_value, LDL_character))
+
 interface()
 
