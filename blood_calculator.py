b4e6e5dbec674f06308679c9b54ad1a2a9d22d5d
Brian Bosem <bab97@duke.edu>
2021-08-30 12:49:55 -0400

    Modified interface so it runs until quit is chosen

--- blood_calculator.py
+++ blood_calculator.py
@@ -1,7 +1,13 @@
 def interface():
     print("Blood calculator")
-    print("Make a choice")
-    print("9 - Quit")
-    choice = input("Make a choice: ")
+    keep_running = True
+    while keep_running:
+        print("Make a choice")
+        print("9 - Quit")
+        choice = int(input("Make a choice: "))
+        if choice == 9:
+            keep_running = False
     print(choice)
     return choice
+
+interface()
