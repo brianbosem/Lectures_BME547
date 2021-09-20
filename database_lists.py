f31a2f2264cc6fc4515104bfea1478e1f6a8bd2a
Brian Bosem <bab97@duke.edu>
2021-09-20 13:15:05 -0400

    added classes for patient structure

--- database_lists.py
+++ database_lists.py
@@ -1,5 +1,27 @@
+class Patient:
+
+    def __init__(self, name, id_no, age):
+        self.name = name
+        self.id_no = id_no
+        self.age = age
+        self.tests = []
+    
+    def __repr__(self):
+        return "{}: {}".format(self.id_no, self.name)
+
+
+def class_work():
+    new_patient = Patient("Ann Ables", 120, 36)
+    print(new_patient.id_no)
+    print(new_patient.name)
+    x = Patient("Bob Boyles", 24, 33)
+    print(x.name)
+    print(x)
+
+
 def create_database_entry(patient_name, id_no, age):
-    new_patient = {"name": patient_name, "id_no": id_no, "age": age, "tests": [],}
+    new_patient = Patient(patient_name, id_no, age)
+    # new_patient = {"name": patient_name, "id_no": id_no, "age": age, "tests": [],}
     # new_patient = [patient_name, id_no, age, []]
     return new_patient
 
@@ -27,8 +49,8 @@ def print_database5(db):
 
 def print_elders(db, age_limit):
     for patient in db:
-        if patient[2] > age_limit:
-            print(patient[0])
+        if patient["age"] > age_limit:
+            print(patient["name"])
 
 def get_patient(db, id):
     patient = db[id]
@@ -39,25 +61,30 @@ def get_patient(db, id):
     #        return patient
 
 def main():
+    # class_work()
+
+    # return
+
+
     db = {}
     x = create_database_entry("Ann Ables", 120, 30)
-    db[x["id_no"]] = x
+    db[x.id_no] = x
     x = create_database_entry("Bob Boyles", 24, 31)
-    db[x["id_no"]] = x
+    db[x.id_no] = x
     x = create_database_entry("Chris Chou", 33, 33)
-    db[x["id_no"]] = x
+    db[x.id_no] = x
     x = create_database_entry("David Dinkins", 14, 34)
-    db[x["id_no"]] = x
+    db[x.id_no] = x
     print(db)
 
     patient_id_tested = 24
     test_done = ("HDL", 65)
 
     patient = get_patient(db, patient_id_tested)
-    patient["tests"].append(test_done)
+    patient.tests.append(test_done)
     print(db)
 
-    # print_database5(db)
+    #print_database5(db)
     # y = db[len[db]-1] old way of finding last entry
     # y = db[-1] # python way of finding last entry, can get second to last etc. 
     # y = db[1:3]
